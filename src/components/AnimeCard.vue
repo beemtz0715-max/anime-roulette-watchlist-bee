18305e60c0648b3e5c6c365f633f1cb7d0ef3a52
beemtz0715-max <beemtz0715@gmail.com>
2026-03-04 10:59:06 -0500

    added anime card loading

---
+++ src/components/AnimeCard.vue
@@ -0,0 +1,29 @@
+<script setup>
+const props = defineProps({
+  anime: {
+    type: Object,
+    default: null,
+  },
+  loading: {
+    type: Boolean,
+    default: false,
+  },
+  error: {
+    type: String,
+    default: '',
+  },
+  inWatchlist: {
+    type: Boolean,
+    default: false,
+  },
+})
+</script>
+
+<template>
+  <section
+    class="Oborder-slate-700/70 Obg-slate-900/60 rounded-3xl border p-5 shadow-2xl shadow-slate-950/30 backdrop-blur-sm"
+  >
+    <div v-if="props.loading">Loading ...</div>
+    <div v-else>Done</div>
+  </section>
+</template>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,18 +1,41 @@
-<script setup></script>
+<script setup>
+import AnimeCard from '@/components/AnimeCard.vue'
+const spin = () => {
+  console.log('Hello')
+}
+</script>
 
 <template>
-<main
-class="min-h-screen bg-[radial-gradient(circle_at_15_20%,#334155, transparent_40%), radial-gradient(circle_at_85%_0%,#0f76
+  <main
+    class="min-h-screen bg-[radial-gradient(circle_at_15%_20%,#334155,transparent_40%),radial-gradient(circle_at_85%_0%,#0f766e,transparent_40%)]"
+  >
+    <div class="mx-auto max-w-7xl">
+      <header class="mb-8"></header>
 
-<div class="mx-auto max-w-7xl">
-<header>
-<p class="text-xs font-semibold tracking-[0.3em]
-<h1 class="mt-2 text-4xl font-black text-white sm:text-5xl">Anime Roulette Machine</h1>
-<p class="mt-2 max-w-3xl text-sm text-slate-300 sm:text-base">
-Spin the reel, request a random anime from Jinkan with VueUse useFech, and learnhow REST
-APIs signal rate limiting with HTTP 429.
-</p>
-</header>
-</div>
-</main>
+      <div class="grid gap-6 lg:grid-cols-[1.2fr_0.8fr]">
+        <section class="space-y-5">
+          <div
+            class="rounded-3xl border border-slate-700/70 bg-slate-900/60 p-5 shadow-2xl shadow-slate-950/30 backdrop-blur"
+          >
+            <div class="flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
+              <div>
+                <h2 class="text-xl font-bold text-white">Roulette</h2>
+                <p class="text-sm text-slate-300">
+                  Pull the lever for your next random anime recommendation.
+                </p>
+              </div>
+
+              <button
+                type="button"
+                class="cursor-pointer rounded-full border border-cyan-300/70 bg-cyan-400/20 px-6 py-3 text-base font-black"
+                @click="spin"
+              >
+                Spin
+              </button>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  </main>
 </template>
